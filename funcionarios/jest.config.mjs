58356171d53eb60e5e25edc394cb97e8c5ea1df8
PedroCB11 <pedro.consoli@gec.inatel.br>
2025-09-28 15:12:00 -0300

    adicionando html report na pipeline

--- funcionarios/jest.config.mjs
+++ funcionarios/jest.config.mjs
@@ -4,5 +4,18 @@ export default {
   transform: {},
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'text', 'lcov']
+  coverageReporters: ['html', 'text', 'lcov'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        outputPath: 'test-report/jest-report.html',
+        pageTitle: 'Test Report',
+        includeFailureMsg: true,
+        includeSuiteFailure: true,
+        sort: 'status'
+      }
+    ]
+  ]
 };
